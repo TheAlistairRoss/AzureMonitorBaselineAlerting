28d5b6b50ec57e1798df539fb7ccfd2d592cc0c3
TheAlistairRoss <45040511+TheAlistairRoss@users.noreply.github.com>
2020-09-16 14:42:56 +0100

    Add files via upload

---
+++ AlertTemplates/DeployAzAlerts.ps1
@@ -0,0 +1,113 @@
+param (
+    [string]
+    $subscriptions,
+
+    [string]
+    $resourceGroupName,
+
+    [string]
+    $resourceGroupLocation
+)
+
+# This needs to be update if automating with a service principal / app registration / managed identity
+# Connect-AzAccount 
+
+# Root Path
+$oTemplateRootPath = Get-Location
+
+# Subscription Check
+$oAllSubscriptions = Get-AzSubscription
+if ($oAllSubscriptions.count -eq 0 -or !($oAllSubscriptions)){
+    Write-Error -Message "No Subscriptions found. Check Account Permissions"
+    exit
+}
+
+if ($subscriptions){
+    $oSubscriptionsFilter = $subscriptions.Split(",") | ForEach-Object {$_.Trim()}
+    $oSubscriptions = $oAllSubscriptions | where {$_.Id.ToString() -In $oSubscriptionsFilter}
+    if ($oSubscriptions.count -lt 1){
+            Write-Error -Message "No Subscriptions filtered from the input parameters.`n'$subscriptions'"
+    }
+    else {
+        Write-Information "$($oSubscriptions.count) subscription(s) found"
+    }
+}
+else {
+    $oSubscriptions = $oAllSubscriptions
+}
+$oSubscriptions
+
+foreach ($oSubscription in $oSubscriptions)
+{
+    Set-AzContext -Subscription $oSubscription 
+    
+    $oSubscriptionScope = "/subscriptions/$($oSubscription.Id)"
+
+    # Ensure the resource group exists, otherwise deploy it
+    $oResourceGroup = Get-AzResourceGroup -Name $resourceGroupName
+    if (!($oResourceGroup))
+    {
+        Write-Information "Creating Resource Group"
+        Try{
+        New-AzResourceGroup -ResourceGroupName $resourceGroupName -Location $resourceGroupLocation -OutVariable $oResourceGroup -ErrorAction Stop
+        }
+        catch{
+            $Error[0]
+            Write-Error -Message "Failed to deploy Resource Group '$($resourceGroupName). Ending deployment for subscription '($($oSubscription.Id)"
+            Break
+        }
+    }
+    else {
+        Write-Information "Resource Group Found"
+        $oResourceGroup
+        $resourceGroupLocation = $oResourceGroup.Location
+    }
+
+    # Get All Resources in the subscription
+    $oResources = Get-AzResource
+
+    
+    <# Need to have a think about the logic as a central workspace will hold all the alerts.
+    # Workspace Deployments
+    $oWorkspaces = $oResources | Where-Object -Property type -Like "Microsoft.OperationalInsights/workspaces"
+    if ($oWorkspaces)
+    {
+        foreach ($oWorkspace in $oWorkspaces)
+        {
+            $i = 1
+            $oParams = @{
+                "workspaceId"       = $oWorkspace.Id
+                "workspaceLocation" = $oWorkspace.Location
+            }
+
+            New-AzResourceGroupDeployment -ResourceGroupName $oResourceGroup.ResourceGroupName `
+                -TemplateFile "$($oPath)\Workspaces\azuredeploy.json" `
+                -TemplateParameterObject $oParams
+            $i++
+        }
+    }
+
+    # Log Alerts - To be deployed to the same location as a workspace
+    #>
+    # Virtual Machines
+    $oVirtualMachines = $oResources | Where-Object ResourceType -Like "Microsoft.Compute/virtualMachines"
+        
+    if ($oVirtualMachines)
+    {
+        $oVMPath = "$oTemplateRootPath\microsoft.compute_virtualmachines"
+        Set-Location $oVMPath
+
+        $DeployAzVMMetricsAlertsHashArguments = @{
+            virtualMachines          = $oVirtualMachines
+            multiResourceMetricScope = $oSubscriptionScope
+            resourceGroupName        = $oResourceGroup.ResourceGroupName
+        }      
+        .\DeployAzVMMetricsAlerts.ps1 @DeployAzVMMetricsAlertsHashArguments
+    }
+
+
+}
+
+
+
+
---
+++ AlertTemplates/microsoft.compute_virtualmachines/DeployAzVMMetricsAlerts.ps1
@@ -0,0 +1,50 @@
+[CmdletBinding()]
+param (
+    [Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource[]]
+    $virtualMachines,
+
+    [string]
+    $multiResourceMetricScope,
+
+    [string]
+    $resourceGroupName
+
+)
+
+# Validate the input
+if ($virtualMachines){
+    $oVirtualMachines = $virtualMachines | Where-Object ResourceType -Like "Microsoft.Compute/virtualMachines"
+    if ($oVirtualMachines.count -lt 1){
+        Write-Error -Message "No Virtual Machines found" -RecommendedAction Stop
+        Exit
+    }
+}   
+Write-Information "$($oVirtualMachines.count) Virtual Machines found"
+
+
+# Get Virtual Machines
+$oVirtualMachinesLocations = @()
+$oVirtualMachines.Location | Select-Object -Unique | Foreach-Object {$oVirtualMachinesLocations += $_}
+Write-Information "$($oVirtualMachinesLocations.count) unique locations for Virtual Machines found"
+$oVirtualMachinesLocations
+
+# Multi Resource Metrics
+# These should have 1 standard alert per region. Need to build some validation to identify whether a deployment is needed
+
+$oParamsFile = Get-Content -Path "microsoft.compute_virtualmachines_baseline_alerts_azure-deploy.parameters.json" | ConvertFrom-Json -Depth 10
+$oparamsFile.parameters.locations.value = $oVirtualMachinesLocations 
+$oParamsFile.parameters.multiResourceMetricAlertScope.value = $multiResourceMetricScope
+
+# Export the converted Parameters file, otherwise the Powershell script needs to declare each parameter
+$oParamsFile | ConvertTo-Json -Depth 10 -EnumsAsStrings | Out-File -FilePath "microsoft.compute_virtualmachines_baseline_alerts_azure-deploy.parameters.json"
+
+# Compile the arguments to a hashtable
+$HashArguments = @{
+    ResourceGroupName     = $resourceGroupName 
+    TemplateFile          = "microsoft.compute_virtualmachines_baseline_alerts_azure-deploy.json" 
+    TemplateParameterFile = "microsoft.compute_virtualmachines_baseline_alerts_azure-deploy.parameters.json"
+}
+
+# Deploy
+Write-Information "Deploying Virtual Machine Metrics Template"
+New-AzResourceGroupDeployment @HashArguments
